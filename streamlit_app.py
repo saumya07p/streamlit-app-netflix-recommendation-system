e3fd01e659a0f33e4eb45be1d9d16bc047943040
Saumya Chandrakant Prasad <100471966+saumya07p@users.noreply.github.com>
2024-10-05 01:21:55 +0000

    updated file

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from pathlib import Path
 st.title("Netflix Recommendation System")
 st.title("Google Sheets Data Dashboard")
 
-url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR88GrAY-eMPSXwVXOwoEABuu2z7D2cRGA58tfZLvCkbskX8zVzpmRQnuK2z5PHL7H9zJ2n4VK7fE4w/pub?gid=2145904198&single=true&output=csv"  # Replace with your URL
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR88GrAY-eMPSXwVXOwoEABuu2z7D2cRGA58tfZLvCkbskX8zVzpmRQnuK2z5PHL7H9zJ2n4VK7fE4w/pub?gid=2145904198&single=true&output=csv"
 
 data = pd.read_csv(url)
 
