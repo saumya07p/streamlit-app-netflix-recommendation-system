5cefba04f001aa540e84f3f3929502e7ca564e56
Saumya Chandrakant Prasad <100471966+saumya07p@users.noreply.github.com>
2024-10-24 06:06:09 +0000

    updated code

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,42 +7,28 @@ import pandas as pd
 st.title("Netflix Recommendation System")
 st.text('You are my sunshine')
 
-# Function to load credentials from Streamlit secrets
 def load_credentials():
     return st.secrets["GOOGLE_SHEETS_CREDS"]
 
-# Function to authorize and connect to Google Sheets
 def connect_to_google_sheets(user_data):
     # Define the scope
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-    # Load credentials from Streamlit secrets
     creds_dict = load_credentials()
-
-    # Create credentials object directly
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-
-    # Authorize client
     client = gspread.authorize(creds)
-
-    # Open the Google Sheet
     sheet = client.open(user_data).sheet1
 
     return sheet
 
-# Function to read data into a DataFrame
 def load_data(sheet):
     data = sheet.get_all_records()
     return pd.DataFrame(data)
 
-# Streamlit app main function
 def main():
     st.title("Google Sheets Data Viewer")
+    SHEET_NAME = "user_data"
     
-    # Replace with your actual Google Sheets name
-    SHEET_NAME = "Project R&D (Responses)"
-    
-    # Connect to the Google Sheets
     try:
         sheet = connect_to_google_sheets(SHEET_NAME)
         df = load_data(sheet)
@@ -56,5 +42,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-fig = px.bar(x=['A', 'B', 'C'], y=[10, 20, 30], title="Simple Bar Chart")
-st.plotly_chart(fig)
+num_rows, num_cols = df.shape
+st.write(f"Number of Rows: {num_rows}")
+st.write(f"Number of Columns: {num_cols}")
+
