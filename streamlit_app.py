4344bff358450874006e2cc3790f40a4f1821675
Saumya Chandrakant Prasad <SaumyaChandrakant.Prasad01@student.csulb.edu>
2024-10-24 03:49:39 +0000

    Updated streamlit_app.py from Colab

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.title("Netflix Recommendation System")
 st.text('You are my sunshine')
+
+fig = px.bar(x=['A', 'B', 'C'], y=[10, 20, 30], title="Simple Bar Chart")
+st.plotly_chart(fig)
