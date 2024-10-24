99e773c82a5abd397d9042538aa3935634baca4c
Saumya Chandrakant Prasad <SaumyaChandrakant.Prasad01@student.csulb.edu>
2024-10-24 03:18:19 +0000

    Updated streamlit_app.py from Colab

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+!pip install plotly
+import plotly.express as px
 
-st.title("sunshine")
+st.title("Netflix Recommendation System")
+st.text('You are my sunshine')
+
+
+device_usage = px.bar(df, x = 'Which mode do you prefer to watch movies?')
+
+device_usage.update_layout(xaxis_title='Device',
+                           yaxis_title='Count')
+
+device_usage.show()
