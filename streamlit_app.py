46f4a7313291955b1f856bfb1276652b0065700c
Saumya Chandrakant Prasad <SaumyaChandrakant.Prasad01@student.csulb.edu>
2024-10-24 03:02:20 +0000

    Updated streamlit_app.py from Colab

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # Your updated Streamlit code here
 import streamlit as st
 
-st.title("Updated Netflix Recommendation System")
+st.title("Netflix Recommendation System")
 # Add more of your Streamlit app code here
