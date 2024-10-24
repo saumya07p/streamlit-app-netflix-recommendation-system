57aaa2c14b7d816e7564eb41a77f46d3aec3d38c
Saumya Chandrakant Prasad <100471966+saumya07p@users.noreply.github.com>
2024-10-24 03:44:25 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import plotly.express as px
 
 st.title("Netflix Recommendation System")
 st.text('You are my sunshine')
-
-
-device_usage = px.bar(df, x = 'Which mode do you prefer to watch movies?')
-
-device_usage.update_layout(xaxis_title='Device',
-                           yaxis_title='Count')
-
-device_usage.show()
