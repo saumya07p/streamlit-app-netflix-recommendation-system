3361ee5252304d4b2c163f4db2df98c9e0582d22
Saumya Chandrakant Prasad <100471966+saumya07p@users.noreply.github.com>
2024-10-30 11:15:15 +0000

    updating UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
+import pandas as pd
 import plotly.express as px
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
 
 st.title("Netflix Recommendation System Dashboard")
 
@@ -49,6 +49,48 @@ def main():
              color_discrete_sequence=px.colors.qualitative.Set2)
 
     fig.update_layout(xaxis_title='Age Group', yaxis_title='Satisfaction Level')
+    st.plotly_chart(fig)
+    
+
+    fig = px.bar(df, x='What is your age group?',
+             title='Distribution of Age Groups',
+             color_discrete_sequence=px.colors.qualitative.Set2)
+
+    fig.update_layout(xaxis_title="Age Group",
+                  yaxis_title="Count",
+                  xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+    movie_genre_counts = px.bar(df, x = 'Which one of the following genres do you prefer to watch? (Select your top most favorite)',title = 'Genre Count')
+
+    movie_genre_counts.update_layout(
+    xaxis_title='Genre',
+    yaxis_title='Genre Count',
+    title={
+    'text': 'Satisfaction with Streaming Recommendations',
+    'y': 0.9,
+    'x': 0.5,
+    'xanchor': 'center',
+    'yanchor': 'top'},
+    xaxis=dict(showgrid=False),
+    yaxis=dict(showgrid=False)
+)
+#st.plotly_chart(movie_genre_counts)
+
+    fig = px.histogram(df,
+                   x='How satisfied are you with the recommendations you receive from streaming platforms?',
+                   nbins=4,
+                   title='Satisfaction with Streaming Recommendations',
+                   color_discrete_sequence=['coral'])
+
+    fig.update_layout(xaxis_title='Satisfaction Level',
+                  yaxis_title='Count',
+                   title={
+        'text': 'Satisfaction with Streaming Recommendations',
+        'y': 0.9,  # Title position
+        'x': 0.5,  # Center the title
+        'xanchor': 'center',
+        'yanchor': 'top'})
     st.plotly_chart(fig)
 
 if __name__ == "__main__":
