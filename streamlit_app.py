bf8e7822e354108f9cd2ce8975b300557fe199ce
Saumya Chandrakant Prasad <100471966+saumya07p@users.noreply.github.com>
2024-11-05 07:32:13 +0000

    renamed columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,10 +34,23 @@ def main():
     if 'Timestamp' in df.columns:
         df = df.drop('Timestamp', axis=1)
 
+    df.rename(columns={'What is your age group?':'Age group',
+                       'Which mode do you prefer to watch movies?': 'Preferred Mode',
+                       'Which one of the following genres do you prefer to watch? (Select your top most favorite)': 'Favorite Genre',
+                       'Which of the following do you use most frequently to choose a streaming platform?': 'Platform Choice Factor',
+                       'On which devices do you primarily watch content?': 'Primary Device',
+                       'How often do you watch or consume content from streaming platforms?': 'Watch Frequency',
+                       'Are you satisfied with the recommendations you receive from streaming platforms?': 'Recommendation Satisfaction',
+                        'How satisfied are you with the recommendations you receive from streaming platforms?': 'Satisfaction Level',
+                        'What prevents you from using Netflix?': 'Netflix Barrier',
+                        'How long do you spend each day watching content on streaming services?': 'Daily Watch Time',
+                        'Does high subscription rate of one platform, forces you to switch to another platform?': 'Switching Due to Cost',
+                        'Kindly give your preference':'Duration preference'},inplace=True)
+    
     df.reset_index(drop=True, inplace=True)
        
     st.dataframe(df)
-    
+
     columns_to_keep = st.multiselect("Select columns to keep:",options=df.columns)
 
     if columns_to_keep:
