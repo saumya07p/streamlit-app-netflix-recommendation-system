56f11178bd70a4c730750594568a4821c53ad1e4
Saumya Chandrakant Prasad <100471966+saumya07p@users.noreply.github.com>
2024-11-28 08:17:21 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 import plotly.graph_objects as go
 import requests
 from io import BytesIO
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 def load_credentials():
         return st.secrets["GOOGLE_SHEETS_CREDS"]
@@ -80,34 +82,31 @@ def dashboard():
     if 'Timestamp' in df.columns:
         df = df.drop('Timestamp', axis=1)
 
-    subs = '1IigjkaGJeY_Dr1I1-mfDRyzIlDImw2rt'
+    FILE_ID = '1IigjkaGJeY_Dr1I1-mfDRyzIlDImw2rt'
 
-    def load_excel(file_id):
-        subs = f'https://docs.google.com/spreadsheets/d/1IigjkaGJeY_Dr1I1-mfDRyzIlDImw2rt/edit?usp=drive_link&ouid=110523202770426482106&rtpof=true&sd=true'
-        response = requests.get(subs)
-        return pd.read_excel(BytesIO(response.content)) if response.status_code == 200 else None
+    url = f'https://drive.google.com/uc?id={FILE_ID}&export=download'
 
-    st.title("Read Excel File from Google Drive")
-    data = load_excel(subs)
-    st.info('HELLO')
-    fig = go.Figure()
+    response = requests.get(url)
+    data = pd.read_excel(BytesIO(response.content), engine='openpyxl')
 
-    fig.add_trace(go.Scatter(
-        x=subs['Year'],
-        y=subs['Subscriptions'],
+    fig1 = go.Figure()
+
+    fig1.add_trace(go.Scatter(
+        x=data['Year'],
+        y=data['Subscriptions'],
         mode='lines+markers',
         name='Subscription'
     ))
 
-    # fig.add_hline(y=167.09, line_dash="dash", line_color="grey", annotation_text="COVID19 Start Year")
-    # fig.add_hline(y=221.84, line_dash="dash", line_color="grey", annotation_text="COVID19 End Year")
+    fig1.add_hline(y=167.09, line_dash="dash", line_color="grey", annotation_text="COVID19 Start Year")
+    fig1.add_hline(y=221.84, line_dash="dash", line_color="grey", annotation_text="COVID19 End Year")
 
-    # fig.update_layout(
-    #     title="Subscription Over Years",
-    #     xaxis_title="Year",
-    #     yaxis_title="Subscription")
+    fig1.update_layout(
+        title="Subscription Over Years",
+        xaxis_title="Year",
+        yaxis_title="Subscription")
 
-    # st.plotly_chart(fig)
+    st.plotly_chart(fig1)
 
     df.rename(columns={'What is your age group?':'Age group',
                        'Which mode do you prefer to watch movies?': 'Preferred Mode',
@@ -129,19 +128,21 @@ def dashboard():
     col1, col2 = st.columns((10,10))
 
     movie_loc = df['Preferred Mode'].value_counts()
-    fig1 = go.Figure(data=[go.Bar(
+    fig2 = go.Figure(data=[go.Bar(
         x=movie_loc.index,
         y=movie_loc.values,
-        marker_color=['#1f77b4', '#ff7f0e'] 
+        marker_color=['#ff4d4d', '#ff9999'] 
     )])
 
-    fig1.update_layout(
-        title="Preferred Modes of Watching Movies",
+    fig2.update_layout(
         xaxis_title="Mode",
         yaxis_title="Count",
-        # xaxis_tickangle=-45
+        title = {
+        'text':'Preferred Modes of Watching Movies',
+        'x': 0.3  
+        } 
     )
-    col1.plotly_chart(fig1)
+    col1.plotly_chart(fig2)
 
     platform_preference = df['Platform Choice Factor'].value_counts()
 
@@ -150,36 +151,69 @@ def dashboard():
 
     platform_df = platform_df.sample(frac=1).reset_index(drop=True)
 
-    fig2 = px.scatter(platform_df,
+    fig3 = px.scatter(platform_df,
                     x="Platform", 
                     y="Count",
                     size="Count",
                     color="Count",
                     hover_name="Platform",
-                    title="Platform Preference Bubble Chart",
-                    labels={"Platform": "Platform", "Count": "Count of Responses"},
+                    title="Platform Preference",
+                    labels={"Platform": "Platform", "Count": "Count of Responses",
+                    },
+                    color_continuous_scale=[
+                        "#ff9999",  # Light red
+                        "#ff4d4d",  # Medium red
+                        "#e60000",  # Dark red
+                        "#990000"   # Deepest red
+                    ],
                     size_max=70)
-    
-    fig2.update_layout(
-        width=600
+
+    fig3.update_layout(
+        width=600,
+        xaxis_tickangle=90,
+        title={
+        'text': 'Platform Preference',
+        'y': 0.9,
+        'x': 0.4,
+        'xanchor': 'center',
+        'yanchor': 'top'}
     )
 
-    col2.plotly_chart(fig2)
+    col2.plotly_chart(fig3)
 
-    genre_count=df['Platform Choice Factor'].value_counts()
-    fig3 = px.histogram(df, 
+    fig4 = px.histogram(df, 
                     x="Favorite Genre",
                     color="Gender",
                     category_orders={"Favorite Genre": df['Favorite Genre'].unique()},
                     title="Genre Preferences by Gender", 
                     labels={"Favorite Genre": "Genre Preference", "count": "Count of Users"},
                     barmode="stack",
-                    
                     )
 
-    st.plotly_chart(fig3)
+    fig4.update_layout(
+        title={
+        'text': 'Platform Preference',
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+    )
 
-    fig5 = px.bar(df, x = 'Favorite Genre',title = 'Genre Count', color = 'Favorite Genre', color_discrete_sequence=px.colors.qualitative.Set2)
+    st.plotly_chart(fig4)
+
+    fig5 = px.bar(df, x = 'Favorite Genre',
+                  title = 'Genre Count', 
+                  color = 'Favorite Genre', 
+                  color_continuous_scale=[
+                    "#660000",  # Very dark red
+                    "#800000",  # Dark red
+                    "#990000",  # Red
+                    "#b30000",  # Medium dark red
+                    "#cc3333",  # Bright red
+                    "#e60000",  # Strong red
+                    "#ff6666",  # Light red
+                    "#ff9999"   # Very light red
+                    ])
     fig5.update_layout(
     xaxis_title='Genre',
     yaxis_title='Genre Count',
@@ -189,15 +223,28 @@ def dashboard():
     'y': 0.9,
     'x': 0.5,
     'xanchor': 'center',
-    'yanchor': 'top'}
+    'yanchor': 'top'},
+    xaxis={
+        'categoryorder': 'total descending'
+    }
    )
     col2.plotly_chart(fig5)
 
+    df_sorted = df['Satisfaction Level'].value_counts().sort_values(ascending=False)
+
     fig6 = px.histogram(df,
                    x='Satisfaction Level',
-                   nbins=4,
-                   title='Satisfaction with Streaming Recommendations',
-                   color_discrete_sequence=px.colors.qualitative.Set2)
+                   color = 'Satisfaction Level',
+                   nbins=6,
+                   title='Streaming Recommendations Satisfaction',
+                                color_discrete_sequence=[
+                        "#8B0000",  # Dark red
+                        "#B22222",  # Firebrick
+                        "#DC143C",  # Crimson
+                        "#FF6347",  # Tomato
+                        "#FF9999"   # Light red
+    ]
+)
 
     fig6.update_layout(xaxis_title='Satisfaction Level',
                   yaxis_title='Count',
@@ -206,7 +253,9 @@ def dashboard():
         'y': 0.9,
         'x': 0.5,
         'xanchor': 'center',
-        'yanchor': 'top'})
+        'yanchor': 'top'},
+        xaxis={'categoryorder': 'total descending'}
+        )
     col1.plotly_chart(fig6)
 
     movie_loc1 = df['Switching Due to Cost'].value_counts()
@@ -220,7 +269,7 @@ def dashboard():
         title="Preferred Modes of Watching Movies",
         xaxis_title="Mode",
         yaxis_title="Count",
-        xaxis_tickangle=-45
+        xaxis={'categoryorder': 'total descending'}
     )
 
     st.plotly_chart(fig7)
@@ -230,26 +279,110 @@ def dashboard():
     ratings=df['Daily Watch Time'].value_counts()
     ratings_df = pd.DataFrame(ratings).reset_index()
     ratings_df.columns = ['Watch Hours', 'Count']
-    col3.dataframe(ratings_df)
+    col3.dataframe(ratings_df, use_container_width=True)
+
+    # html_table = ratings_df.to_html(index=False, justify='center', border=0)
+
+    # col5, col6 = st.columns([2,2])
+
+    # with col5:
+    #     st.markdown("<h3 style='text-align: center;'>Daily Watch Time Statistics</h3>", unsafe_allow_html=True)
+
+    #     styled_df = ratings_df.style.set_table_styles(
+    #         [
+    #         {"selector": "th", "props": [("text-align", "center"), ("font-size", "16px"), ("font-weight", "bold")]},  # Center header and style
+    #         {"selector": "td", "props": [("text-align", "center"), ("padding", "8px")]}  # Center table data and add padding
+    #         ]
+    #     )
+
+    # with col5:
+    #     st.table(styled_df)
 
     pair_counts = df.groupby(['Watch Frequency', 'Daily Watch Time']).size().reset_index(name='count')
     pair_counts.sort_values(by='count',ascending=False).head(3)
-    col4.dataframe(pair_counts)
+    col4.dataframe(pair_counts, use_container_width=True)
 
 def model1():
 
-    SHEET_NAME = "user_data"
-    sheet = connect_to_google_sheets(SHEET_NAME)
-    df = load_data(sheet)
-   
-    if 'Timestamp' in df.columns:
-        df = df.drop('Timestamp', axis=1)
+    st.title('Content based Movie Recommendation System')
 
-    st.markdown("<h1 style='text-align: center;'>Recommendation Model</h1>", unsafe_allow_html=True)
+    file_id2 = '1SoHSIAK4Mx9QNjwxaJQ0WNcwvOD6ioJT'
 
-    genre_options = df['Which one of the following genres do you prefer to watch? (Select your top most favorite)'].unique()
+    url = f'https://drive.google.com/uc?id={file_id2}'
 
-    st.selectbox('Select your Preferred Genre',genre_options)
+    movies_df1 = pd.read_csv(url, encoding='latin1')
+
+    st.write('A Content-Based Recommendation System is a type of recommendation system that suggests items to users based on the features or characteristics of the items themselves, rather than user behavior or interactions (which is the case in collaborative filtering). The system analyzes the content or attributes of the items and recommends similar items that match the user past preferences.')
+
+    xyz = movies_df1['Genre'].unique()
+    recomm1 = st.selectbox('Select your Preferred Genre', xyz)
+
+    def reco(genre):
+        filter=movies_df1[movies_df1['Genre']==genre]
+        filter_final=filter.sort_values(by='IMDB Score', ascending= False)
+        filter_final.reset_index(inplace=True)
+        filter_final=filter_final[['Title','Runtime','IMDB Score']]
+        filter_final=filter_final.head(10)
+        # filter_final.drop('index', inplace=True)
+        return filter_final
+    
+    movies_list=reco(recomm1)
+    st.dataframe(movies_list, use_container_width=True)
+    st.write("We are currently in the process of collecting data for our academic research project, and your participation would be incredibly valuable to us. By completing this form, you will contribute to the success of our study, and we would greatly appreciate your time and input. Your responses will help us gain insights that are essential for our research. Thank you in advance for your effort and support! : You can access the Google Form by clicking on the link below:")
+    st.page_link('https://forms.gle/ZDd8UQP9qVykxMz99', label = 'https://forms.gle/ZDd8UQP9qVykxMz99')
+
+def model2():
+
+    file_id = '1hc3q0Kt7SK-QCrL5Az-jpDJdKtaYrQda'
+
+    url = f'https://drive.google.com/uc?id={file_id}'
+
+    movies_df = pd.read_csv(url)
+    
+    st.title('Context Based Movie Recommendation System')
+    st.write('Description-based recommendation uses the textual content of movie descriptions to find similarities between movies. It compares the description of a given movie with others using techniques like TF-IDF and cosine similarity to quantify how similar they are. The system then suggests the top 10 movies with the most similar descriptions to the input movie.')
+
+    abc = movies_df['title'].unique()
+    recomm = st.selectbox('Select your Preferred Movie', abc)
+
+    movies_df = movies_df[['title', 'cast', 'description']].fillna('')
+
+    movies_df['combined_features'] = movies_df['cast'] + " " + movies_df['description']
+
+    tfidf = TfidfVectorizer(stop_words='english')
+
+    tfidf_matrix = tfidf.fit_transform(movies_df['combined_features'])
+
+    # Compute cosine similarity matrix based on the combined features
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+    # Function to recommend movies based on title input
+    def recommend_movies(movie_title, cosine_sim=cosine_sim):
+        # Get the index of the movie that matches the title
+        idx = movies_df[movies_df['title'] == movie_title].index[0]
+        
+        # Get pairwise similarity scores of all movies with that movie
+        sim_scores = list(enumerate(cosine_sim[idx]))
+        
+        # Sort the movies based on the similarity scores
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        
+        # Get the scores of the 10 most similar movies
+        sim_scores = sim_scores[1:11]  # Exclude the first one as it is the same movie
+        
+        # Get the movie indices
+        movie_indices = [i[0] for i in sim_scores]
+        
+        # Return the top 10 most similar movies
+        return movies_df['title'].iloc[movie_indices]
+
+    recommendations = recommend_movies(recomm)
+    df_movies = pd.DataFrame(recommendations).reset_index()
+    df_movies.drop('index', axis = 1, inplace =True)
+    st.dataframe(df_movies, use_container_width=True)
+
+    st.write("We are currently in the process of collecting data for our academic research project, and your participation would be incredibly valuable to us. By completing this form, you will contribute to the success of our study, and we would greatly appreciate your time and input. Your responses will help us gain insights that are essential for our research. Thank you in advance for your effort and support! : You can access the Google Form by clicking on the link below:")
+    st.page_link('https://forms.gle/ZDd8UQP9qVykxMz99', label = 'https://forms.gle/ZDd8UQP9qVykxMz99')
     
 if ott=='Home':
     home()
@@ -257,3 +390,5 @@ elif ott=='Dashboard':
      dashboard()
 elif ott == 'Model1':
     model1()
+elif ott == 'Model2':
+    model2()
